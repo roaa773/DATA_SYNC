751faeb819cfabd011ad10d7ac76e46042fbff22
roaa773 <roaa.ahmed7733@gmail.com>
2026-09-10 01:47:27 +0300

    Update DATA_SYNC.v

--- src/DATA_SYNC.v
+++ src/DATA_SYNC.v
@@ -2,10 +2,14 @@
 module tt_um_data_sync #(parameter BUS_WIDTH = 8,
 	               parameter NUM_STAGES = 2)
 (
+	input  wire       ena,      // always 1 when the design is powered, so you can ignore it
 	input clk,   
 	input rst,
 	input [BUS_WIDTH-1:0] unsync_bus,
 	input bus_enable,
+	input  wire [6:0] uio_in,   // IOs: Input path
+	output wire [6:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
 	output reg [BUS_WIDTH-1:0] sync_bus,
 	output reg enable_pulse  
 );
@@ -15,6 +19,11 @@ reg enable_ff;
 wire enable_pulse_reg;
 wire [BUS_WIDTH-1:0] sync_bus_reg;
 
+wire _unused = &{ena,uio_in, 1'b0};
+	
+assign uio_out = 0;
+assign uio_oe  = 0;	
+	
 assign enable_pulse_reg = sync_reg[NUM_STAGES-1] && !enable_ff;
 assign sync_bus_reg = enable_pulse_reg ? unsync_bus: sync_bus;
 
