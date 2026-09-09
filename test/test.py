8060975748f1406c0a5e37c20b6a11502c16579b
roaa773 <roaa.ahmed7733@gmail.com>
2026-09-10 00:29:07 +0300

    Change clock period unit from us to ns

--- test/test.py
+++ test/test.py
@@ -11,7 +11,7 @@ async def test_project(dut):
     dut._log.info("Start")
 
     # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, unit="us")
+    clock = Clock(dut.clk, 10, unit="ns")
     cocotb.start_soon(clock.start())
 
     # Reset
@@ -19,9 +19,9 @@ async def test_project(dut):
     dut.ena.value = 1
     dut.ui_in.value = 0
     dut.uio_in.value = 0
-    dut.rst_n.value = 0
+    dut.rst.value = 0
     await ClockCycles(dut.clk, 10)
-    dut.rst_n.value = 1
+    dut.rst.value = 1
 
     dut._log.info("Test project behavior")
 
