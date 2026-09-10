e92cfebeea3e0cf9d136afa313ea4bb8400ab162
roaa773 <roaa.ahmed7733@gmail.com>
2026-09-10 03:08:24 +0300

    Change reset signal from rst to rst_n

--- test/test.py
+++ test/test.py
@@ -19,9 +19,9 @@ async def test_project(dut):
     dut.ena.value = 1
     #dut.ui_in.value = 0
     #dut.uio_in.value = 0
-    dut.rst.value = 0
+    dut.rst_n.value = 0
     await ClockCycles(dut.clk, 10)
-    dut.rst.value = 1
+    dut.rst_n.value = 1
 
     dut._log.info("Test project behavior")
 
