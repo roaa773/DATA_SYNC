ea95cda52503aa658629e19c173347b485f5567b
roaa773 <roaa.ahmed7733@gmail.com>
2026-09-10 03:46:01 +0300

    Update DATA_SYNC.v

--- src/DATA_SYNC.v
+++ src/DATA_SYNC.v
@@ -5,13 +5,11 @@ module tt_um_data #(parameter BUS_WIDTH = 8,
 	input  wire       ena,      // always 1 when the design is powered, so you can ignore it
 	input clk,   
 	input rst_n,
-	input [BUS_WIDTH-1:0] unsync_bus,
-	input bus_enable,
-	input  wire [6:0] uio_in,   // IOs: Input path
-	output wire [6:0] uio_out,  // IOs: Output path
+	input [BUS_WIDTH-1:0] ui_in,
+	input  wire [7:0] uio_in,   // IOs: Input path
+	output wire [7:0] uio_out,  // IOs: Output path
     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-	output reg [BUS_WIDTH-1:0] sync_bus,
-	output reg enable_pulse  
+	output reg [BUS_WIDTH-1:0] uo_out
 );
 
 reg [NUM_STAGES-1:0] sync_reg;
@@ -25,7 +23,7 @@ assign uio_out = 0;
 assign uio_oe  = 0;	
 	
 assign enable_pulse_reg = sync_reg[NUM_STAGES-1] && !enable_ff;
-assign sync_bus_reg = enable_pulse_reg ? unsync_bus: sync_bus;
+assign sync_bus_reg = enable_pulse_reg ? ui_in: uo_out;
 
 
 always @(posedge clk or negedge rst_n)
@@ -36,7 +34,7 @@ always @(posedge clk or negedge rst_n)
    end
   else
    begin
-    sync_reg <= {sync_reg[NUM_STAGES-2:0],bus_enable};
+    sync_reg <= {sync_reg[NUM_STAGES-2:0],uio_in[7]};
    end  
  end
 
@@ -51,19 +49,19 @@ end
 
 always @(posedge clk or negedge rst_n) begin 
 	if(~rst_n) begin
-		enable_pulse <= 0;
+		uio_out <= 0;
 	end 
 	else begin
-		enable_pulse <= enable_pulse_reg;
+		uio_out <= enable_pulse_reg;
 	end
 end
 
 always @(posedge clk or negedge rst_n) begin 
 	if(~rst_n) begin
-		sync_bus <= 0;
+		uo_out <= 0;
 	end 
 	else begin
-		sync_bus <= sync_bus_reg;
+		uo_out <= sync_bus_reg;
 	end
 end
 
