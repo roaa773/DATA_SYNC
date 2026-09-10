e58cdf33cf9d1abcc44109f254c29440e295fa00
roaa773 <roaa.ahmed7733@gmail.com>
2026-09-10 03:50:55 +0300

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,8 +28,8 @@ module tb ();
      .ena (ena),
       .ui_in  (ui_in),    // Dedicated inputs
       .uo_out (uo_out),   // Dedicated outputs
-     .uio_in (uio_in[7:0]),   // IOs: Input path
-     .uio_out(uio_out[7:0]),  // IOs: Output path
+     .uio_in (uio_in),   // IOs: Input path
+     .uio_out(uio_out),  // IOs: Output path
      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
       .clk    (clk),      // clock
       .rst_n  (rst_n)     // not reset
